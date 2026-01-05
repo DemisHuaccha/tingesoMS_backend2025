a2518a8f647e36255cc8cc4c0055dea581a03551
Demis <demis.huaccha@usach.cl>
2026-01-04 23:22:15 -0300

    xd

---
+++ tingesoMS_backend/tingesoMS_fee/src/main/java/com/tingeso/tingesoMS_fee/Repository/ToolFeeRepository.java
@@ -0,0 +1,7 @@
+package com.tingeso.tingesoMS_fee.Repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ToolFeeRepository {
+}
--- tingesoMS_backend/tingesoMS_fee/src/test/java/com/tingeso/tingesoMS_fee/Service/FeeServiceTest.java
+++ tingesoMS_backend/tingesoMS_fee/src/test/java/com/tingeso/tingesoMS_fee/Service/FeeServiceTest.java
@@ -1,8 +1,5 @@
 package com.tingeso.tingesoMS_fee.Service;
 
-import com.tingeso.tingesoMS_fee.Entities.ToolFee;
-import com.tingeso.tingesoMS_fee.Repository.ToolFeeRepository;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -16,30 +13,4 @@ import static org.mockito.Mockito.*;
 @ExtendWith(MockitoExtension.class)
 class FeeServiceTest {
 
-    @Mock
-    private ToolFeeRepository repo;
-
-    @InjectMocks
-    private FeeService service;
-
-    @Test
-    void save() {
-        ToolFee fee = new ToolFee();
-        fee.setToolId(1L);
-        fee.setLoanFee(1000);
-        when(repo.save(any(ToolFee.class))).thenReturn(fee);
-        
-        ToolFee result = service.save(fee);
-        assertEquals(1000, result.getLoanFee());
-    }
-
-    @Test
-    void getFee() {
-        ToolFee fee = new ToolFee();
-        fee.setToolId(1L);
-        when(repo.findById(1L)).thenReturn(Optional.of(fee));
-        
-        ToolFee result = service.getFee(1L);
-        assertNotNull(result);
-    }
 }
