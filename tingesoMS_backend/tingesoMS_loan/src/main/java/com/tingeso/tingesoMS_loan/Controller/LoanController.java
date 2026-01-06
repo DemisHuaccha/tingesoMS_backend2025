3dd5d53c4f2fe1ea0726eb4b1ac672404de89624
Demis <demis.huaccha@usach.cl>
2026-01-06 16:30:26 -0300

    Fix crossorigins

--- tingesoMS_backend/tingesoMS_loan/src/main/java/com/tingeso/tingesoMS_loan/Controller/LoanController.java
+++ tingesoMS_backend/tingesoMS_loan/src/main/java/com/tingeso/tingesoMS_loan/Controller/LoanController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/loan")
 public class LoanController {
